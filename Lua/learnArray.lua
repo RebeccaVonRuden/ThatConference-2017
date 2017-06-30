1917feb05d8e2120000b5c3f134e9fa96fa5e74f
RebeccaVonRuden <rebecca@vonruden.info>
2017-06-29 20:48:56 -0500

    practice loops and arrays

--- Lua/learnArray.lua
+++ Lua/learnArray.lua
@@ -1,10 +1,13 @@
 
-
 function main()
     print("Define empty array")
     local arr = {}
     for i = 1, 25 do
         arr[i] = i
+    end
+    print("Print array info")
+    for i = 1, 25 do
+        print(arr[i])
     end
 end
 
