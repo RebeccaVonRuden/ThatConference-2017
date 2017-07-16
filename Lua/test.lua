e97692b02645277f7d7ba319d3a1e27a62236a4a
Jason Von Ruden <jvonruden@gmail.com>
2017-07-15 20:26:31 -0500

    remove test

--- Lua/test.lua
+++
@@ -1,9 +0,0 @@
--- Computercraft has built in integration with pastebin
--- pastebin get dzwc8Vu0 buildCabin
-
--- You will need minecraft to create your own that cabin
-
-Minecraft : minecraft.net
-
-
-https://pastebin.com/vUw4CqrL
