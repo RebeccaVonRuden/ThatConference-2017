f4b0d26c90a6a238a1a001b94b17f040bef36cee
Jason Von Ruden <jvonruden@gmail.com>
2017-07-15 20:58:49 -0500

    More Requirements

--- Minecraft/BuildCabinFinal.lua
+++ Minecraft/BuildCabinFinal.lua
@@ -1,11 +1,32 @@
 --[[
-	Presentation: LEARNING TO PROGRAM USING MINECRAFT IN-GAME COMPUTERS AND TURTLES USING THE LUA PROGRAMMING LANGUAGE
-	Author: Rebecca Von Ruden
-	Link: https://www.thatconference.com/sessions/session/11175
+  Presentation: LEARNING TO PROGRAM USING MINECRAFT IN-GAME COMPUTERS AND TURTLES USING THE LUA PROGRAMMING LANGUAGE
+  Author: Rebecca Von Ruden
+  Link: https://www.thatconference.com/sessions/session/11175
 
-  ***************************************
-  ** PasteBin: dzwc8Vu0
-  **************************************
+  You will need: Mining Turtle, Lava Bucket, Oak Wood Planks, Stone Slabs, Glass Blocks, and Oak Wood Door
+
+  Requirements:
+     1. Place items in “Mining Turtle” with Lava Bucket in inventory slot 1.
+     2. Refuel Turtle using inventory slot 1 and rest of the items anywhere.
+     3. Function to Find Block’s Slot in the Turtle’s inventory.
+     4. Function to Place Block Down.
+	 5. Create function build_block_wall(number_of_blocks).
+     6. Using a for loop to place down 4 blocks down in a row using Place Block Down function.
+     7. Create function build_story(wall_length).
+     8. Using a for loop to build 4 block_walls using left turns using build_block_wall function.
+     9. Create function build_stories(number_of_stories, wall_length).
+    10. Using a for loop to build 3 stories with a wall length of 5.
+    11. Using the build_story function.
+    12. Create a function to Place Roof Down using stone slabs.
+    13. Create a for loop and if i % 2 == 0 then to know when to turn left or right when placing the roof block down in a function.
+    14. Create a function to position turtle and Dig out wall blocks and Place the door.
+    15. Create a function to position turtle and dig out wall blocks and Place the window.
+    16. Update the main(number_of_stories,wall_length) function to accept 2 arguments.
+    17. Pass the arguments to the new main(args[1],args[2]) function.
+
+  ***********************************************
+  ** PasteBin: pastebin get dzwc8Vu0 buildCabin
+  ***********************************************
 
 --]]
 
